bffdcb6c891926f9a105ca3c8af16ab4cc51e081
Fernando Aureliano <mail@fernandoaureliano.com>
2024-09-18 16:45:45 -0300

    example test

--- tests/integration/test_service_two_integration.py
+++ tests/integration/test_service_two_integration.py
@@ -3,7 +3,7 @@ from services import service_two
 
 def test_user_structure():
     """
-    Test the structure of a user.
+    Test the structure of a user-.
     """
     users = service_two.get_users()
     user = users[0]
